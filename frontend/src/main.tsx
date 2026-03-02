3c96c3e4cbf075d2d9caf2f204da771368c2cfc5
dunt21 <suadmacaulay50@gmail.com>
2026-03-02 01:20:18 +0000

    feat: Implement initial login and forgot password pages with basic routing and project setup.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-import ReactDOM from "react-dom/client";
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
 
-const router = createRouter({ routeTree });
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
